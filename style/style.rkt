6fd2601b97be84126ce2e60716209c2ce6982f57
chihyang <chihyanghsin@gmail.com>
2019-10-05 23:02:57 +0800

    Add css to styles.

--- style/style.rkt
+++ style/style.rkt
@@ -1,13 +1,15 @@
 #lang racket
-(require scribble/core scribble/latex-properties)
-
-(define right
-  (make-style "Sright" (list (make-tex-addition "../style/right.tex"))))
-
-(define underline
-  (make-style "Sunderline" (list (make-tex-addition "../style/underline.tex"))))
+(require scribble/core scribble/latex-properties scribble/html-properties)
 
 (define question
-  (make-style "Squestion" (list (make-tex-addition "../style/question.tex"))))
+  (make-style "Squestion" (list (make-tex-addition "../style/question.tex")
+                                (make-css-addition "../style/question.css"))))
+(define right
+  (make-style "Sright" (list (make-tex-addition "../style/right.tex")
+                             (make-css-addition "../style/right.css"))))
+
+(define underline
+  (make-style "Sunderline" (list (make-tex-addition "../style/underline.tex")
+                                 (make-css-addition "../style/underline.css"))))
 
 (provide right underline question)
